8eea6c8963478904cfccd59633f111d157b60073
ITWaves-Nikhil <nikhil.itwaves@gmail.com>
2023-12-04 18:27:34 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import PushNotification from 'react-native-push-notification';
+
+const App = () => {
+  useEffect(() => {});
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
